60d4f9f9432cc6ea18b0a79c198c564592e3fdb2
Raisson Souza <raissonrai@gmail.com>
2024-10-10 23:55:53 -0300

    Reimplementada tela de controle dos leds.

--- src/components/Led/index.tsx
+++ src/components/Led/index.tsx
@@ -1,12 +1,13 @@
-import { Button, Text, View } from "react-native"
+import { Button, Text, View, StyleSheet } from "react-native"
 import React, { useEffect, useState } from "react"
 import Service from "../../services/Service"
 
 type LedProps = {
     ledName: string
+    ledApiName: string
 }
 
-export const Led: React.FC<LedProps> = ({ ledName }) => {
+export const Led: React.FC<LedProps> = ({ ledName, ledApiName }) => {
     const [ lightsOn, setLightsOn ] = useState<boolean>(false)
     const [ loading, setLoading ] = useState<boolean>(false) // TODO: ajustar após rota de GET
 
@@ -14,24 +15,45 @@ export const Led: React.FC<LedProps> = ({ ledName }) => {
         const fetchLights = async () => {
 
         }
+        fetchLights()
     }, [])
 
     const changeLights = async () => {
-        await Service.SetLed("", true)
+        await Service.SetLed("", true) // TODO: ledApiName aqui
     }
 
     return (
-        <View>
+        <View style={ styles.container }>
             {
                 loading
-                    ? <Text>Carregando informações do ESP 32</Text>
-                    : (
+                    ? <Text style={ styles.text }>Carregando informações do ESP 32</Text>
+                    : <>
+                        <Text style={ styles.text }>Led { ledName }</Text>
                         <Button
-                            title={ lightsOn ? `Desligar led ${ ledName }` : `Ligar led ${ ledName }` }
+                            title={ lightsOn ? "Desligar" : "Ligar" }
                             onPress={ () => changeLights() }
                         />
-                    )
+                    </>
             }
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 10,
+        borderColor: "grey",
+        borderRadius: 15,
+        borderStyle: "solid",
+        borderWidth: 1,
+        padding: 10,
+        width: "50%",
+    },
+    text: {
+        color: "white",
+        fontSize: 20,
+        fontWeight: "bold",
+    }
+})
--- src/screens/Leds.tsx
+++ src/screens/Leds.tsx
@@ -1,10 +1,29 @@
 import { Led } from "../components/Led"
 import { Screen } from "../components/base/Screen"
+import { View, StyleSheet } from "react-native"
+
 
 export const Leds: React.FC<{}> = () => {
     return (
         <Screen>
-            <Led ledName="x" />
+            <View style={ styles.container }>
+                <Led ledName="Quarto01" ledApiName="" />
+                <Led ledName="Banheiro" ledApiName="" />
+                <Led ledName="Quarto02" ledApiName="" />
+                <Led ledName="Corredor" ledApiName="" />
+                <Led ledName="Garagem" ledApiName="" />
+                <Led ledName="Externo" ledApiName="" />
+            </View>
         </Screen>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: 20,
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
