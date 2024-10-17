2914065083d6bc3ced6f6c1d77494c8906315d43
Raisson Souza <raissonrai@gmail.com>
2024-10-17 19:33:14 -0300

    Tela dos leds refatorada.

--- src/screens/Leds.tsx
+++ src/screens/Leds.tsx
@@ -1,19 +1,30 @@
 import { Led } from "../components/Led"
 import { Screen } from "../components/base/Screen"
-import { View, StyleSheet } from "react-native"
-
+import { StyleSheet,ScrollView } from "react-native"
 
 export const Leds: React.FC<{}> = () => {
     return (
         <Screen>
-            <View style={ styles.container }>
+            <ScrollView
+                style={{
+                    flex: 1,
+                    width: "100%"
+                }}
+                contentContainerStyle={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: 20,
+                    marginTop: 10,
+                    marginBottom: 10,
+                }}
+            >
                 <Led ledName="Quarto01" ledApiName="" />
                 <Led ledName="Banheiro" ledApiName="" />
                 <Led ledName="Quarto02" ledApiName="" />
                 <Led ledName="Corredor" ledApiName="" />
                 <Led ledName="Garagem" ledApiName="" />
                 <Led ledName="Externo" ledApiName="" />
-            </View>
+            </ScrollView>
         </Screen>
     )
 }
